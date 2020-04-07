1b6d502b5285b926bdd4cf1362029aedff25c2bd
rileylgray <grayril@sheridancollege.ca>
2020-04-07 15:05:00 -0400

    about me section

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
    $('.scroll-work').click(function(){
       $('html, body').animate({scrollTop: $('.jswork').offset().top},1000);
    });
+    $('.scroll-me').click(function(){
+      $('html, body').animate({scrollTop: $('.jsabout').offset().top},1000);
+   });
 });
