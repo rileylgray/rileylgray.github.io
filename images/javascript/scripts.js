413be22574cbd2fc7194ea434848b63a710e99ec
rileylgray <grayril@sheridancollege.ca>
2020-08-27 13:04:52 -0400

    update

--- images/javascript/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-   $('.scroll-work').click(function(){
-      $('html, body').animate({scrollTop: $('.jswork').offset().top},1000);
-   });
-    $('.scroll-me').click(function(){
-      $('html, body').animate({scrollTop: $('.jsabout').offset().top},1000);
-   });
-});
